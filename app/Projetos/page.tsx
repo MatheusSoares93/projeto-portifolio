8d426a935bd962e1c64cdb3399ea51ae0546fb62
MatheusSoares93 <142833871+MatheusSoares93@users.noreply.github.com>
2023-11-27 17:51:04 +0000

    CommitFinal

--- app/Projetos/page.tsx
+++ app/Projetos/page.tsx
@@ -1,9 +1,11 @@
+import Image from 'next/image'
+import print from '../img/print.png'
 export default function Projetos (){
     return (
         <section className="Projetos" id="Projetos">
         <h2 className="TituloProjetos">Conheça Alguns dos Meus <span>Projetos</span></h2>
         <div>
-
+            <Image className='Projeto1' src={print} alt=''/>
         </div>
 
     </section>  
